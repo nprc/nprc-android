7d1de34bf8511357b5f543aef700947a2ab3d8cb
Peter Chapman <peter@conglomo.co.nz>
2015-05-20 17:19:48 +1200

    Added support for the new website.

--- app/src/main/java/nz/co/conglomo/nprc/Website.java
+++ app/src/main/java/nz/co/conglomo/nprc/Website.java
@@ -1,9 +1,12 @@
 package nz.co.conglomo.nprc;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class Website extends Activity {
@@ -13,8 +16,24 @@ public class Website extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_website);
         WebView myWebView = (WebView)findViewById(R.id.webview);
+        myWebView.setWebViewClient(new MyWebViewClient());
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        myWebView.loadUrl("http://nprc.nz/android.html");
+        myWebView.loadUrl("http://nprc.nz/");
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("nprc.nz")) {
+                // This is my web site, so do not override; let my WebView load the page
+                return false;
+            }
+
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
     }
 }
